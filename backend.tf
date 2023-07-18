5f56691783304af736be5c457d51b04272319450
giftyakua <126066254+giftyakua@users.noreply.github.com>
2023-07-18 15:01:34 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "jay-jay-1"
-    key = "jay-jay.tfstate"
-    region = "us-east-1"
-  }
-}
+#terraform {
+ # backend "s3" {
+  #  bucket = "jay-jay-1"
+   # key = "jay-jay.tfstate"
+    #region = "us-east-1"
+  #}
+#}

e644ba1d031709f97c83324e8affd83314852392
giftyakua <126066254+giftyakua@users.noreply.github.com>
2023-07-18 14:41:05 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,49 +1,148 @@
-# Configure the AWS provider
 provider "aws" {
   region = var.aws_region
 }
 
-# Use the VPC module
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  version = "3.0.0"
-
-  name = var.vpc_name
-  cidr = var.vpc_cidr
-
-  azs             = var.vpc_availability_zones
-  private_subnets = var.private_subnets
-  public_subnets  = var.public_subnets
-
-  enable_nat_gateway = var.enable_nat_gateway
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
 
   tags = {
-    Terraform = "true"
-    Environment = "production"
+    Name = var.vpc_name
   }
 }
 
-# Launch a public EC2 instance
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = format("%s-public-%d", var.vpc_name, count.index + 1)
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = format("%s-private-%d", var.vpc_name, count.index + 1)
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = format("%s-igw", var.vpc_name)
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = format("%s-public-rt", var.vpc_name)
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "example" {
+  name_prefix = "example"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "public_instance" {
   ami           = var.public_ami
   instance_type = var.instance_type
-  subnet_id     = module.vpc.public_subnets[0]
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id     = aws_subnet.public[0].id
+  vpc_security_group_ids = [aws_security_group.example.id]
 
   tags = {
-    Name = "PublicEC2Instance"
+    Name = format("%s-public-ec2", var.vpc_name)
   }
 }
 
-# Launch a private EC2 instance
 resource "aws_instance" "private_instance" {
   ami           = var.private_ami
   instance_type = var.instance_type
-  subnet_id     = module.vpc.private_subnets[0]
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id     = aws_subnet.private[0].id
+  vpc_security_group_ids = [aws_security_group.example.id]
 
   tags = {
-    Name = "PrivateEC2Instance"
+    Name = format("%s-private-ec2", var.vpc_name)
   }
 }
 
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_internet_gateway.gw.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = format("%s-nat-gateway", var.vpc_name)
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = format("%s-private-rt", var.vpc_name)
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}

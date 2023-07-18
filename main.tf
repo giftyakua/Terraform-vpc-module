ca34866ead3d076b2a0850f4251afb510eeef25c
giftyakua <126066254+giftyakua@users.noreply.github.com>
2023-07-18 15:56:52 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -110,21 +110,21 @@ resource "aws_instance" "private_instance" {
   }
 }
 
-#resource "aws_nat_gateway" "nat" {
- # allocation_id = aws_internet_gateway.gw.id
-  #subnet_id     = aws_subnet.public[0].id
-#
- # tags = {
-  #  Name = format("%s-nat-gateway", var.vpc_name)
-  #}
-#}
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_internet_gateway.gw.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = format("%s-nat-gateway", var.vpc_name)
+  }
+}
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-   # nat_gateway_id = aws_nat_gateway.nat.id
+    nat_gateway_id = aws_nat_gateway.nat.id
   }
 
   tags = {

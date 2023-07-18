439b71dc2c472297ae3ecfe261e2e5d558815290
giftyakua <126066254+giftyakua@users.noreply.github.com>
2023-07-18 14:49:27 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
-}
+#output "public_subnet_ids" {
+ # value = aws_subnet.public[*].id
+#}
 
-output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
-}
+#output "private_subnet_ids" {
+#  value = aws_subnet.private[*].id
+#}
